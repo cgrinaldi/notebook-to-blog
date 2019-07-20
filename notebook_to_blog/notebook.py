2e846256f4753d0b5ddcfc2cd7567d885ea4b8e3
cgrinaldi <cgrinaldi@gmail.com>
2019-07-20 12:21:33 -0700

    Initial work on notebook

---
+++ notebook_to_blog/notebook.py
@@ -0,0 +1,6 @@
+class Notebook:
+    def __init__(self, path):
+        self.path = path
+
+    def convert(self):
+        return ""
---
+++ notebook_to_blog/tests/test_notebook.py
@@ -0,0 +1,16 @@
+import pytest
+
+from notebook_to_blog.notebook import Notebook
+
+
+@pytest.fixture()
+def notebook():
+    return Notebook("my-path")
+
+
+def test_notebook_has_path(notebook):
+    assert notebook.path == "my-path"
+
+
+def test_notebook_converts_to_string(notebook):
+    assert isinstance(notebook.convert(), str)
