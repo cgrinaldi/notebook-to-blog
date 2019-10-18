3fcaecaf0c9feb8753548f8afcdaf420629892c1
cgrinaldi <cgrinaldi@gmail.com>
2019-10-17 19:28:19 -0700

    Update documentation for Notebook class

--- notebook_to_blog/notebook.py
+++ notebook_to_blog/notebook.py
@@ -5,7 +5,26 @@ from notebook_to_blog.constants import PROJECT_DIR
 
 
 class Notebook:
+    """
+    Notebook class that is responsible for loading Jupyter notebook file
+    contents and delegating conversion of cell type to the respective
+    cell classes.
+
+    Result of calling the .convert() method is a string representing
+    the converted Notebook to a string (which can be written to a file).
+
+    Currently, cell classes supported are:
+    - MarkdownCell
+    - CodeCell
+    """
+
     def __init__(self, path, output_dir=None, gh_creds=None):
+        """
+        Args:
+            path: str, path to Jupyter notebook to convert
+            output_dir: str, path to directory to output files to
+            gh_creds: str, path to file containing Github credentials
+        """
         self.path = path
         self.output_dir = output_dir
         self.gh_creds = gh_creds
@@ -17,6 +36,7 @@ class Notebook:
         self._load_cells()
 
     def _load_cells(self):
+        """Helper method to load up all cells depending on cell_type."""
         self.cells = []
         for i, x in enumerate(self.contents["cells"]):
             if x["cell_type"] == "markdown":
@@ -29,6 +49,7 @@ class Notebook:
         return self.cells
 
     def convert(self):
+        """Convert notebook by converting all of Notebook's cells."""
         return "\n\n".join([c.convert() for c in self.cells])
 
 
